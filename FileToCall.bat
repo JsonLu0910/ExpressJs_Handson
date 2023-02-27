c043f8723b4823882be9f34ac5b53f89dc8fbaef
JsonLu0910 <jasonlu0910@gmail.com>
2023-02-27 11:46:31 +0800

    Testing on Batch Script

--- FileToCall.bat
+++ FileToCall.bat
@@ -1,3 +1,7 @@
+@echo off
+setlocal
+
+echo Running the try block...
 echo on
 
 for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%y%%x%%z
@@ -11,3 +15,14 @@ Echo zipping...
 echo Done!
 
 ftp -i -s:FileZillaClient.bat
+
+set "errorlevel="
+( command1 && command2 && command3 ) || set "errorlevel=1"
+
+REM Catch block
+if defined errorlevel (
+y    REM Error handling code goes here
+    echo An error occurred. Stopping the script...
+    exit /b 1
+)
+
--- FileZillaClient.bat
+++ FileZillaClient.bat
@@ -1,6 +1,23 @@
+@echo off
+setlocal
+
+echo Running the try block...
 open 0.tcp.ap.ngrok.io 17346
 user
 123
 lcd C:\Users\arkmind\Documents\GitHub
 put ExpressJSHandson.zip
 bye
+REM Try block
+
+set "errorlevel="
+( command1 && command2 && command3 ) || set "errorlevel=1"
+
+REM Catch block
+if defined errorlevel (
+    echo An error occurred. Handling the error...
+    REM Error handling code goes here
+    bye
+    exit /b 1
+
+)
