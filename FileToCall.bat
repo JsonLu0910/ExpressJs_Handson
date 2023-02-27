db429e6cbae6bb50721b7b5567bd702b42334df7
JsonLu0910 <jasonlu0910@gmail.com>
2023-02-27 15:44:43 +0800

    Testing on Batch Script

--- FileToCall.bat
+++ FileToCall.bat
@@ -12,7 +12,7 @@ Echo zipping...
 
 "C:\Program Files\7-Zip\7z.exe" a -tzip "C:\Users\arkmind\Documents\GitHub\ExpressJSHandson.zip" "C:\Users\arkmind\Documents\GitHub\ExpressJs_Handson"
 
-echo Done!
+echo done!
 
 ftp -i -s:FileZillaClient.bat
 
